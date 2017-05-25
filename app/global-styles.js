37b485d3fb218d662d10ddd9a27505b883a9a483
Heyde Moura <moura.heyde@gmail.com>
2017-05-25 20:21:38 -0300

    :lipstick: Global styles for content

---
+++ app/global-styles.js
@@ -0,0 +1,13 @@
+export default {
+  content: {
+    display: 'flex',
+    flex: 1,
+    justifyContent: 'space-between',
+    alignItems: 'stretch',
+    alignContent: 'center',
+    paddingHorizontal: 25,
+    paddingVertical: 30,
+    width: null,
+    height: null
+  }
+}
